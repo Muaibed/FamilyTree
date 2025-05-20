2978bb058e16462675bc2c418af226db9b497f4a
Muaibed <j.muaibd@gmail.com>
2025-05-20 19:17:10 +0300

    add family name automatically

--- src/components/client/CreatePersonForm.tsx
+++ src/components/client/CreatePersonForm.tsx
@@ -87,7 +87,10 @@ const CreatePersonForm = ({
 
         <select
           value={fatherId ?? ""}
-          onChange={(e) => setFatherId(e.target.value)}
+          onChange={(e) => {
+            setFatherId(e.target.value)
+            setFamilyName(members.people[e.target.value].familyName)
+          }}
           className="w-full px-4 py-2 border border-gray-300 dark:border-gray-600 rounded-md bg-white dark:bg-gray-700 text-gray-800 dark:text-white focus:outline-none focus:ring-2 focus:ring-blue-500"
         >
           <option value="">Select Father</option>
