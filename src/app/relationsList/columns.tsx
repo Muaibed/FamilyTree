606f36a95a0a51c04873404611836f21f629f320
Muaibed <j.muaibd@gmail.com>
2025-07-14 00:08:42 +0300

    delete a spouseRelationship has been added to relationsList

--- src/app/relationsList/columns.tsx
+++ src/app/relationsList/columns.tsx
@@ -2,11 +2,16 @@
 
 import { ColumnDef } from "@tanstack/react-table";
 import { SpouseRelationship } from "@/generated/prisma";
+import { Button } from "@/components/ui/button";
 
 // This type is used to define the shape of our data.
 // You can use a Zod schema here if you want.
 
-export function columns(): ColumnDef<SpouseRelationship>[] {
+export function columns({
+  remove,
+}: {
+  remove: (relation: SpouseRelationship) => void;
+}): ColumnDef<SpouseRelationship>[] {
   return [
     {
       accessorKey: 'id',
@@ -23,6 +28,21 @@ export function columns(): ColumnDef<SpouseRelationship>[] {
     {
       accessorKey: "isActive",
       header: "isActive",
+    },
+    {
+      id: "actions",
+      cell: ({ row }) => {
+        const request = row.original;
+        return (
+          <Button
+            variant="ghost"
+            className="h-8 w-8 p-0 hover:cursor-pointer"
+            onClick={() => remove(request)}
+          >
+            Delete
+          </Button>
+        );
+      },
     },
   ];
 }
