ba071ca421dc2e35569790ef4b5575b5152f8691
Muaibed <j.muaibd@gmail.com>
2025-09-05 01:13:08 +0300

    fix: duplication children when the mother and grandmother is from the family but the father is not

--- src/lib/tree.ts
+++ src/lib/tree.ts
@@ -1,16 +1,25 @@
 import { PersonWithRelations } from "@/types/family";
 import { TreeNode } from "@/types/tree";
 
+let addedMembers: any[] = [];
 export const prepareTreeData = (
   members: PersonWithRelations[],
   startId: string,
   familyId: string,
+  prevNodeGender?: string,
 ): TreeNode | undefined => {
   const person = members.find(p => p.id === startId);
 ;
   if (!person) return undefined;
 
-  const node: TreeNode = {
+  let node: TreeNode | null = null;
+  if (!addedMembers.find(id => id === person.id)) {
+    let grandmother;
+    if (prevNodeGender === "FEMALE")
+      grandmother = members.find(p => p.id === person.father?.motherId)
+
+    if (!prevNodeGender || (prevNodeGender === "FEMALE" && person.father?.familyId !== familyId && grandmother?.familyId !== familyId) || prevNodeGender === "MALE") {
+      node = {
         name: person.firstName,
         attributes: {
           id: person.id,
@@ -18,10 +27,14 @@ export const prepareTreeData = (
         },
         children: [],
       };
+    }
+  }
+
+  addedMembers.push({"id" : person.id})
 
   if (person.gender === "MALE" && person.fatherChildren.length > 0) {
     person.fatherChildren.forEach((childId) => {
-      const childNode = prepareTreeData(members, childId.id, familyId);
+      const childNode = prepareTreeData(members, childId.id, familyId, person.gender);
       if (node && childNode) {
         node.children.push(childNode);
       }
@@ -29,15 +42,14 @@ export const prepareTreeData = (
   }
 
   if (person.gender === "FEMALE" && person.motherChildren.length > 0) {
-    if (person.father?.familyId !== familyId) {
       person.motherChildren.forEach((childId) => {
-        const childNode = prepareTreeData(members, childId.id, familyId);
+        const childNode = prepareTreeData(members, childId.id, familyId, person.gender);
         if (node && childNode) {
           node.children.push(childNode);
         }
       });
-    }
   }
 
-  return node;
+  if (node)
+    return node;
 };
