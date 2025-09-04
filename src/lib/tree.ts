a3063543e93e8a0b83eec89a3d0c9eb10044e7aa
Muaibed <j.muaibd@gmail.com>
2025-09-05 00:29:17 +0300

    fix: repeating children on both parents

--- src/lib/tree.ts
+++ src/lib/tree.ts
@@ -3,37 +3,40 @@ import { TreeNode } from "@/types/tree";
 
 export const prepareTreeData = (
   members: PersonWithRelations[],
-  startId: string
+  startId: string,
+  familyId: string,
 ): TreeNode | undefined => {
   const person = members.find(p => p.id === startId);
 ;
   if (!person) return undefined;
 
   const node: TreeNode = {
-    name: person.firstName,
-    attributes: {
-      id: person.id,
-      gender: person.gender,
-    },
-    children: [],
-  };
+        name: person.firstName,
+        attributes: {
+          id: person.id,
+          gender: person.gender,
+        },
+        children: [],
+      };
 
   if (person.gender === "MALE" && person.fatherChildren.length > 0) {
     person.fatherChildren.forEach((childId) => {
-      const childNode = prepareTreeData(members, childId.id);
-      if (childNode) {
+      const childNode = prepareTreeData(members, childId.id, familyId);
+      if (node && childNode) {
         node.children.push(childNode);
       }
     });
   }
 
   if (person.gender === "FEMALE" && person.motherChildren.length > 0) {
-    person.motherChildren.forEach((childId) => {
-      const childNode = prepareTreeData(members, childId.id);
-      if (childNode) {
-        node.children.push(childNode);
-      }
-    });
+    if (person.father?.familyId !== familyId) {
+      person.motherChildren.forEach((childId) => {
+        const childNode = prepareTreeData(members, childId.id, familyId);
+        if (node && childNode) {
+          node.children.push(childNode);
+        }
+      });
+    }
   }
 
   return node;
